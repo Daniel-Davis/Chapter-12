a421e9f729d6031176be98db155296333f9c573b
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-18 12:24:22 -0500

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -1,2 +1,15 @@
 // Daniel Davis, insertionSort, class file that holds insertionSort function //
 
+function insertionSort() {
+  var temp, inner;
+  for (var outer = 1, outer <= this.dataStore.length, ++outer) {
+    temp = this.dataStore[outer];
+    inner = outer;
+    while (inner > 0 && (this.dataStore[inner-1] >= temp)){
+      this.dataStore[inner] = this.dataStore[inner-1];
+      --inner;
+    }
+    this.dataStore[inner] = temp;
+    print(this.tostring());
+  }
+}
