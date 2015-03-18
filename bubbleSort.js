abce5ba08f86f766cd9931923651e1649c9b264a
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-18 11:51:43 -0500

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,9 @@
 // Daniel Davis, bubbleSort, function file for bubble sorting //
 
-function bubbleSort() 
+function bubbleSort() {
+  var nunElements = this.dataStore.length;
+  var temp;
+  for (var outer = nunElements, outer >= 2, --outer) {
+
+  }
+}
