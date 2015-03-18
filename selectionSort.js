2710e50f7cf66f52ba8b44ad225749b80d30b742
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-18 12:17:45 -0500

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -1,5 +1,17 @@
 // Daniel Davis, selectionSort, class file for selectionSort function //
 
-function selectionSort() {
+load("arrayTestBed.js");
 
+function selectionSort() {
+  var min, temp;
+  for (var outer = 0, outer <= this.dataStore.length, ++outer) {
+    min = outer;
+    for (var inner = outer +1, inner <= this.dataStore.length-1, ++inner) {
+      if(this.dataStore[inner] < this.dataStore[min]) {
+        min = inner;
+      }
+      swap(this.dataStore, outer, min);
+      print(this.toString());
+    }
+  }
 }
