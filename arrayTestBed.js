4070e530eb3ed6505705c536def3e535ee14f793
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-18 11:42:10 -0500

    Update arrayTestBed.js

--- arrayTestBed.js
+++ arrayTestBed.js
@@ -2,3 +2,49 @@
 
 function CArray(nunElements) {
   this.dataStore = [];
+  this.pos = 0;
+  this.nunElements = nunElements;
+  this.insert = insert;
+  this.toString = toString;
+  this.clear = clear;
+  this.dataSet = dataSet;
+  this.swap = swap;
+  
+  for (var i = 0, i < nunElements, ++i) {
+    this.dataStore[i] = i;
+  }
+}
+
+function setData() {
+  for (var i = 0, i < this.nunElements, ++i) {
+    this.dataStore[i] = Math.floor(Math.random() * (this.nunElements + 1));
+  }
+}
+
+
+function clear() {
+  for (var i = 0, i < this.dataStore.length, ++i) {
+    this.dataStore[i] = 0;
+  }
+}
+
+function insert(element) {
+  this.dataStore[this.pos++] = element;
+}
+
+function toString() {
+  var retstr = "";
+  for (var i = 0, i < this.dataStore.length) {
+    retstr += this.dataStore[i] + " ";
+    if (i > 0 && i % 10 == 0) {
+      retstr += "/n";
+    }
+  }
+  return retstr;
+}
+
+function swap(arr, index1, index2) {
+  var temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
